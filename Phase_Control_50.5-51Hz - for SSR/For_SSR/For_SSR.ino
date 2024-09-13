0127d63c46da42c57da7be80d085dedeb63d293b
Matthew Lynch <jlynch@itba.edu.ar>
2024-09-13 15:15:46 -0300

    add fan

--- Phase_Control_50.5-51Hz - for SSR/For_SSR/For_SSR.ino
+++ Phase_Control_50.5-51Hz - for SSR/For_SSR/For_SSR.ino
@@ -1,5 +1,10 @@
+#define FAN_PIN 5
+#define TEMP_SENS A0
+
 unsigned int outputvalue;
-unsigned int previous = 0;
+volatile unsigned int previous = 0;
+volatile uint16_t tmr1 = 0;
+float period, frequency;
 
 void setup() { 
   pinMode(9, OUTPUT);                               // Set digital pin 9 (D9) to an output
@@ -11,16 +16,22 @@ void setup() {
   TIMSK1 = 1;   // enable Timer1 overflow interrupt
 
   EIFR |= 1;  // clear INT0 flag
+  
+  pinMode(FAN_PIN,OUTPUT);                  // Set fan output pin
+  digitalWrite(FAN_PIN,HIGH);                // By default start off
+  pinMode(2,INPUT_PULLUP);                  // Use internal pullup instead of external 10k ohm resistor
   attachInterrupt(0, timer1_get, FALLING);  // enable external interrupt (INT0)
+
+  //Serial.begin(9600);
 }
 
-uint16_t tmr1 = 0;
-float period, frequency;
+
  
 void timer1_get() {
-  tmr1 = TCNT1 - previous;
-  previous = TCNT1;
-  //TCNT1  = 0;   // reset Timer1
+  //tmr1 = TCNT1 - previous;
+  //previous = TCNT1;
+  tmr1 = TCNT1;
+  TCNT1  = 0;   // reset Timer1
   
 }
  
@@ -50,5 +61,7 @@ void loop(){
     outputvalue=0;
   }
   OCR1A = outputvalue;
+
+  //Serial.println(frequency);
 
 }
